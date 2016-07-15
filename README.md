e7cc9e6233995830680c55c9da4467d7cbb1842b
cvidler <christopher.vidler@dynatrace.com>
2016-07-15 10:57:22 +1000

    collection scripts complete

--- README.md
+++ README.md
@@ -3,6 +3,22 @@ Establishes network reliability and routing profiles amongst a network of AMDs.
 
 
 ## Requires
-MTR v0.85+
-bash
+MTR v0.85+ with the 
+fix_split-mode pull (#51 https://github.com/traviscross/mtr/pull/51) 
+and
+fix to split mode not outputting missed hops
+	diff --git a/split.c b/split.c
+	index 73dc9a3..ad81f7f 100644
+	--- a/split.c
+	+++ b/split.c
+	@@ -140,7 +140,7 @@ void split_open(void)
+	 #endif
+	   LineCount = -1;
+	   for (i=0; i<MAX_LINE_COUNT; i++) {
+	-    strcpy(Lines[i], "???");
+	+    strcpy(Lines[i], "??");
+	   }
+	 }
+	 
+compiled in
 
--- amd_pathping.sh
+++ amd_pathping.sh
@@ -1,7 +1,7 @@
 # amd_pathping Main Script
 # Chris Vidler - Dynatrace DCRUM SME 2016
 #
-# Reads destinations file, and produces data data on netwokr routing and loss.
+# Reads destinations file, and produces data data on network routing and loss.
 #
 
 #config 
@@ -20,12 +20,15 @@ MTU=1500
 
 
 # Start of script - do not edit below
-set -euo pipefail
+#set -euo pipefail
 IFS=$',\n\t'
 AWK=`which awk`
+SORT=`which sort`
+CAT=`which cat`
 JOBS=`which jobs`
 WC=`which wc`
-MTR=`which mtr`
+#MTR=`which mtr`
+MTR=/home/data_mine/mtr/mtr
 if [ $? -ne 0 ]; then echo -e "fatal MTR not found in path/not installed"; exit 1; fi
 
 #command line parameters
@@ -93,6 +96,7 @@ echo -e "amd_pathping script"
 echo 
 echo -e "Loading global configuration from config file: $CFGFILE"
 
+#read global config file
 IFS="=";
 while read a v; do
 	#echo -e "$a:$v"
@@ -116,13 +120,21 @@ while read a v; do
 			#comment/blank lines
 			;;
 		*)
-			echo unknown config line: $a
+			echo -e "\e[33m***WARNING:\e[39m Ignoring unknown config line: $a"
 			;;
 	esac
 done < $CFGFILE
- 
 IFS=$',\n\t'
 
+#mtr needs root for intervals below 1 second, check the config and if root or not.
+#if (( $(echo "$INTERVAL < 1.0" | bc -l) )); then
+#	if [ $EUID -ne 0 ]; then
+#		echo -e "\e[31m***FATAL:\e[39m <1.0 sec Interval requires root."
+#		exit 1
+#	fi
+#fi
+
+#display global configs
 echo -e "  Timeout:  $TIMEOUT"
 echo -e "  Interval: $INTERVAL"
 echo -e "  Max Hops: $MAXHOPS"
@@ -131,23 +143,47 @@ echo -e "  Max MTU:  $MTU"
 echo 
 
 
+#display destinations from config file
 echo -e "Loading destinations to test from config file: $DSTLIST"
 echo
 echo -e "`$AWK -F"," '$1=="A" { print " + " $2 "" } ' $DSTLIST`"
 echo -e "\e[2m`$AWK -F"," '$1=="D" { print " - " $2 " Disabled" } ' $DSTLIST`\e[0m"
 echo
 
-DODEBUG=""
+TIMESTAMP=`date -d "\`date -u +"%Y-%m-%d %H:00:00"\`" +%s` ; TIMESTAMP=`printf "%x" $TIMESTAMP`
+if [ $DEBUG -ne 0 ]; then echo -e "\e[36m***DEBUG: TIMESTAMP=$TIMESTAMP \e[39m"; fi
+OUTFILE="$BASEDIR/pathping_${TIMESTAMP}_e10_t"
+if [ $DEBUG -ne 0 ]; then echo -e "\e[36m***DEBUG: OUTFILE=$OUTFILE \e[39m"; fi
+SRC=`ip addr | grep "state UP" -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'` 
+if [ $DEBUG -ne 0 ]; then echo -e "\e[36m***DEBUG: SRC=$SRC \e[39m"; fi
+
+#output header data to file if not already existing
+if [ ! -r $OUTFILE ]; then echo "#ts src dst hop host loss% rcv snt best avg worst" > $OUTFILE; fi
+
+#iterate through active destinations testing them.
 $AWK -F"," '$1=="A" { print $2 } ' $DSTLIST | ( while read p; do 
+
+	#pause while thread count high
 	while [ $($JOBS -r | $WC -l) -ge $MAXTHREADS ]; do sleep 1; done
-	echo -e "Performing pathping for destination: ${p}"
-	if [ $DEBUG -ne 0 ]; then DODEBUG=-d; fi
-	#$SCRIPTDIR/archiveamd.sh -n "${p}" -u "${q}" -b "$BASEDIR" $DODEBUG &
-	sudo $MTR --report --show-ips --no-dns --mpls --timeout=$TIMEOUT --report-cycles=$TESTS --interval=$INTERVAL --max-ttl=$MAXHOPS --psize=-$MTU "$p"
+
+	#start test (as a background subshell)
+	(
+	echo -e "Performing testing for destination: ${p}"
+	TMPFILE=`mktemp`
+	if [ $DEBUG -ne 0 ]; then echo -e "\e[36m***DEBUG: PID=$BASHPID DEST=$p TMPFILE=$TMPFILE \e[39m"; fi
+	TS=`date -u +%s`; TS=`printf "%x" $TS`
+	sudo $MTR --no-dns --timeout=$TIMEOUT --report-cycles=$TESTS --interval=$INTERVAL --max-ttl=$MAXHOPS --psize=-$MTU --split ${p} > $TMPFILE
+	$CAT $TMPFILE | $SORT -g -k 1,1 -k 4,4 | $AWK -F" " -v ts="$TS" -v src="$SRC" -v dst="$p" ' BEGIN { p = $1 } { if (p != $1) { print ts,src,dst,old }; old = $0; p = $1 } END { print ts,src,dst,old } ' | $AWK ' NF > 3 ' >> $OUTFILE
+	rm $TMPFILE
+	) &
+
 done; wait
 )
 
+#complete
 echo
 echo -e "amd_pathping script complete"
 echo
+if [ $DEBUG -ne 0 ]; then echo -e "\e[36m***DEBUG: Output follows:"; cat $OUTFILE; echo -e "\e[39m"; fi
+
 
